1d5c6868580dabb994fce7401383df2f1f6c3079
adrianliaw <adrianliaw2000@gmail.com>
2017-03-31 02:43:28 +0800

    Add grafana iframe into stage 1

--- app/flow/stage0.js
+++ app/flow/stage0.js
@@ -5,10 +5,12 @@ export default ({moveToNextStage, title, subtitle}) => (
   <div>
     <section className="hero is-primary is-bold is-fullheight">
       <div className="hero-body">
-        <div className="container has-text-centered">
-          <h1 className="title">{title}</h1>
-          <h2 className="subtitle">{subtitle}</h2>
-          <a className="button is-primary is-inverted is-medium is-outlined" onClick={moveToNextStage}>Start Demo</a>
+        <div className="demo-content">
+          <div className="container has-text-centered">
+            <h1 className="title">{title}</h1>
+            <h2 className="subtitle">{subtitle}</h2>
+            <a className="button is-primary is-inverted is-medium is-outlined" onClick={moveToNextStage}>Start Demo</a>
+          </div>
         </div>
       </div>
     </section>
--- app/flow/stage1.js
+++ app/flow/stage1.js
@@ -5,10 +5,15 @@ export default ({moveToNextStage, title, subtitle}) => (
   <div>
     <section className="hero is-primary is-bold is-fullheight">
       <div className="hero-body">
-        <div className="container has-text-centered animated slideOutUp">
-          <h1 className="title">{title}</h1>
-          <h2 className="subtitle">{subtitle}</h2>
-          <a className="button is-primary is-inverted is-medium is-outlined animated fadeOut" onClick={moveToNextStage}>Start Demo</a>
+        <div className="demo-content">
+          <div className="container has-text-centered">
+            <h1 className="title">{title}</h1>
+            <h2 className="subtitle">{subtitle}</h2>
+            <a className="button is-primary is-inverted is-medium is-outlined animated fadeOut" onClick={moveToNextStage}>Start Demo</a>
+          </div>
+          <div className="container has-text-centered animated fadeIn">
+            <iframe src="/grafana/dashboard-solo/db/hyperpilot-demo?panelId=1" width="550" height="300" />
+          </div>
         </div>
       </div>
     </section>
