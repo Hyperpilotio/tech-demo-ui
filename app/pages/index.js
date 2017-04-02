d659a247c8a04738b4b12f91c4b48c65f6f0995b
adrianliaw <adrianliaw2000@gmail.com>
2017-04-03 02:20:42 +0800

    Add actions folder as api

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,24 +1,22 @@
 import React from "react"
 import Head from "next/head"
+import Router from "next/router"
 import Flow from "../flow"
 import classNames from "classnames"
 import stylesheet from 'styles/index.scss'
+import "isomorphic-fetch"
 
 export default class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      demoStage: 0
-    };
-  }
-
-  moveToNextStage() {
-    this.setState({demoStage: this.state.demoStage + 1});
+  static async getInitialProps({ req, query }) {
+    if (req !== undefined)
+      query.stage = 0;
+    console.log(query);
+    return { demoStage: parseInt(query.stage || "0") }
   }
 
   render() {
-    const Stage = Flow[this.state.demoStage];
+    const Stage = Flow[this.props.demoStage];
     return (
       <div>
         <Head>
@@ -26,7 +24,10 @@ export default class App extends React.Component {
           <style dangerouslySetInnerHTML={{ __html: stylesheet  }} />
         </Head>
 
-        <Stage moveToNextStage={::this.moveToNextStage} {...this.state.content} />
+        <Stage moveToNextStage={() => Router.push({
+          pathname: "/",
+          query: { stage: this.props.demoStage + 1 }
+        })} prefetch />
       </div>
     );
   }
---
+++ app/server.js
@@ -0,0 +1,48 @@
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare()
+.then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true)
+    const { pathname, query } = parsedUrl
+    if (pathname.startsWith("/grafana")) {
+      res.end("Will be replaced by a Grafana panel");
+      return
+    }
+    if (pathname.startsWith("/actions/")) {
+      try {
+
+        const handler = require(`.${pathname}`);
+        res.setHeader("Content-Type", "application/json");
+        handler({ req, returnJson: (responseJson) => {
+          res.end(JSON.stringify(responseJson));
+        }});
+
+      } catch (e) {
+
+        if (e.code === "MODULE_NOT_FOUND") {
+          res.statusCode = 404;
+          res.end("404 Page Not Found");
+        } else {
+          res.statusCode = 500;
+          res.end("500 Internal Server Error");
+          console.error(e);
+        }
+
+      } finally {
+        return;
+      }
+    }
+    handle(req, res, parsedUrl);
+  })
+  .listen(3000, (err) => {
+    if (err) throw err
+    console.log('> Ready on http://localhost:3000')
+  })
+})
