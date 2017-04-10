d978cec15f3af70b8f152c0534d1341a63bf2944
adrianliaw <adrianliaw2000@gmail.com>
2017-04-11 02:21:29 +0800

    Make the panels more well-organised

--- components/PanelsLayout.js
+++ components/PanelsLayout.js
@@ -1,12 +1,51 @@
-let Panel = ({dashboard = "hyperpilot-demo", panelId}) => (
+const Panel = ({dashboard = "hyperpilot-demo", panelId}) => (
   <div>
-
     <iframe src={process.env.NODE_ENV === "production" ?
                  `/grafana/dashboard-solo/db/${dashboard}?panelId=${panelId}` :
                  `http://play.grafana.org/dashboard-solo/db/internal-grafana-stats?panelId=${panelId}`} />
-
   </div>
 );
+
+
+const GoDDDRequestsCount = () => (
+  <div>
+    <div>
+      <span className="tag is-dark is-pulled-left">Throughput</span>
+      <span className="tag is-info is-pulled-left">Application QoS</span>
+    </div>
+    <Panel panelId="3" />
+  </div>
+);
+
+const GoDDDLatency = () => (
+  <div>
+    <div>
+      <span className="tag is-dark is-pulled-left">Latency</span>
+      <span className="tag is-info is-pulled-left">Application QoS</span>
+    </div>
+    <Panel panelId="1" />
+  </div>
+);
+
+const SparkJobsFinished = () => (
+  <div>
+    <div>
+      <span className="tag is-dark is-pulled-left">Throughput</span>
+      <span className="tag is-info is-pulled-left">Application QoS</span>
+    </div>
+    <Panel panelId="4" />
+  </div>
+);
+
+const CpuUtilization = () => (
+  <div>
+    <div>
+      <span className="tag is-info is-pulled-left">Resource Utilization</span>
+    </div>
+    <Panel panelId="2" />
+  </div>
+);
+
 
 export default ({children}) => (
   <div>
@@ -21,35 +60,20 @@ export default ({children}) => (
             </div>
             <div className="tile">
               <div className="tile is-parent">
-                <div className="tile is-child box is-blurry is-info has-text-centered">
+                <div className="tile is-child box is-success has-text-centered">
                   <h4 className="subtitle is-4">High Priority Web Application</h4>
-                  <div>
-                    <span className="tag is-dark">Throughput</span>
-                    <span className="tag is-info">Application QoS</span>
-                  </div>
-                  <Panel panelId="3" />
-                  <div>
-                    <span className="tag is-dark">Latency</span>
-                    <span className="tag is-info">Application QoS</span>
-                  </div>
-                  <Panel panelId="1" />
+                  <GoDDDRequestsCount />
+                  <GoDDDLatency />
                 </div>
               </div>
               <div className="tile is-vertical is-parent">
-                <div className="tile is-child box is-vertical is-blurry is-info has-text-centered">
+                <div className="tile is-child box is-vertical is-danger has-text-centered">
                   <h4 className="subtitle is-4">Low Priority Spark Jobs</h4>
-                  <div>
-                    <span className="tag is-dark">Throughput</span>
-                    <span className="tag is-info">Application QoS</span>
-                  </div>
-                  <Panel panelId="4" />
+                  <SparkJobsFinished />
                 </div>
                 <div className="tile is-child box is-vertical is-blurry is-dark has-text-centered">
                   <h4 className="subtitle is-4">Cluster Resources</h4>
-                  <div>
-                    <span className="tag is-info">Resource Utilization</span>
-                  </div>
-                  <Panel panelId="2" />
+                  <CpuUtilization />
                 </div>
               </div>
             </div>
