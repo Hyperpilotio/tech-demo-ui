c8985fcaa6280dca3f9b91becb16e10e05b4aa45
adrianliaw <adrianliaw2000@gmail.com>
2017-04-12 00:16:19 +0800

    Add actions/reset_demo

---
+++ actions/reset_demo.js
@@ -0,0 +1,42 @@
+const { exec } = require("child-process-promise");
+
+module.exports = () => {
+
+  let promises = [];
+
+  for (let ns of ["default", "hyperpilot"]) {
+    // Delete all pods (will recover automatically)
+    promises.push(
+      exec(`kubectl get pods -n ${ns} -o json`)
+        .then(res => Promise.resolve(JSON.parse(res.stdout)))
+        .then(data => Promise.resolve(
+          data.items
+            .filter(pod => pod.metadata.labels.app !== "demo-ui")
+            .map(pod => pod.metadata.name)
+            .join(" ")
+        ))
+        .then(pods => exec(`kubectl delete pods ${pods} -n ${ns}`))
+    );
+  }
+
+  for (let deployment of ["load-controller", "spark-load-controller"]) {
+    // Delete deployments deployed during the demo
+    promises.push(
+      exec(`kubectl delete -f /home/app/scripts/${deployment}-deployment.json`)
+    );
+  }
+
+  // Catch errors by "each" Promise
+  promises = promises.map(prom => prom.catch(reason => Promise.resolve(reason)));
+
+  return Promise.all(promises)
+    .then(values => Promise.resolve(
+      values.map(res => ({ stdout: res.stdout, stderr: res.stderr }))
+    ))
+    .then(values => Promise.resolve({
+      deleteDefaultPods: values[0],
+      deleteHyperpilotPods: values[1],
+      deleteLoadController: values[2],
+      deleteSparkLoadController: values[3]
+    }));
+}
