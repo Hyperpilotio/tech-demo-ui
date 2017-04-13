7d1989cdb7cc8613fc59c783930652c4b6891834
adrianliaw <adrianliaw2000@gmail.com>
2017-04-13 21:38:54 +0800

    Add dev version of get_status_indicators

--- actions/get_status_indicators.js
+++ actions/get_status_indicators.js
@@ -4,18 +4,27 @@ const influx = new Influx.InfluxDB({
   database: "snap"
 });
 
-module.exports = () => (
+module.exports = () => {
 
-  influx.query(`
-    SELECT MEAN(value)
-    FROM "hyperpilot/goddd/api_booking_service_request_latency_microseconds"
-    WHERE summary = 'quantile_50'
-    AND time > now() - 1m`)
-    .then(results => Promise.resolve({
-      app: results[0].mean > 0.01 ? "bad" : "good",
+  if (process.env.NODE_ENV === "production") {
+    return influx.query(`
+      SELECT MEAN(value)
+      FROM "hyperpilot/goddd/api_booking_service_request_latency_microseconds"
+      WHERE summary = 'quantile_50'
+      AND time > now() - 1m`)
+      .then(results => Promise.resolve({
+        app: results[0].mean > 0.01 ? "bad" : "good",
+        spark: "inactive",
+        cpu: "warn",
+        network: "warn"
+      }));
+  } else {
+    return Promise.resolve({
+      app: "good",
       spark: "inactive",
       cpu: "warn",
       network: "warn"
-    }))
+    });
+  }
 
-);
+};
