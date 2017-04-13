94810832ac47839fd0ab34171b8df388cb2f1dbe
adrianliaw <adrianliaw2000@gmail.com>
2017-04-13 21:29:16 +0800

    Query influx to determine if the latency is good or not

--- actions/get_status_indicators.js
+++ actions/get_status_indicators.js
@@ -1,6 +1,21 @@
-module.exports = () => Promise.resolve({
-  app: "good",
-  spark: "inactive",
-  cpu: "warn",
-  network: "warn"
+const Influx = require("influx");
+const influx = new Influx.InfluxDB({
+  host: "influxsrv",
+  database: "snap"
 });
+
+module.exports = () => (
+
+  influx.query(`
+    SELECT MEAN(value)
+    FROM "hyperpilot/goddd/api_booking_service_request_latency_microseconds"
+    WHERE summary = 'quantile_50'
+    AND time > now() - 1m`)
+    .then(results => Promise.resolve({
+      app: results[0].mean > 0.01 ? "bad" : "good",
+      spark: "inactive",
+      cpu: "warn",
+      network: "warn"
+    }))
+
+);
